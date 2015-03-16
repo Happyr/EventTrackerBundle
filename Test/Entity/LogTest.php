fb602fdab94f249fd510bf3f5ff5bd602d84d9a2
Tobias Nyholm <tobias.nyholm@gmail.com>
2015-03-16 13:18:52 +0100

    Added test

--- Test/Entity/LogTest.php
+++ Test/Entity/LogTest.php
@@ -2,10 +2,16 @@
 
 namespace Happyr\EventTrackerBundle\Test\Entity;
 
+use Happyr\EventTrackerBundle\Entity\Log;
+
 /**
  * @author Tobias Nyholm
  */
-class LogTest
+class LogTest extends \PHPUnit_Framework_TestCase
 {
-
+    public function testConstructor()
+    {
+        $log = new Log();
+        $this->assertNotNull($log->getTime(), 'A timestamp should be created with the Log');
+    }
 } 
